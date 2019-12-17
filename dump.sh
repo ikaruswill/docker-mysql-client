533ed8254d0f359f5f331337e94055fe0d6d9c35
Will Ho <ikaruswill.ho@gmail.com>
2019-12-17 16:17:20 +0800

    Refactor mysql dump command

--- dump.sh
+++ dump.sh
@@ -21,6 +21,12 @@ function rotate_dumps {
 	ls -t | grep $pattern | sed -e 1,${BACKUP_RETENTION}d | xargs -d '\n' -I {} sh -c 'echo "Deleting $1" && rm -r $1 >/dev/null 2>&1' sh {}
 }
 
+function dump_db {
+	echo "$db: Dumping database"
+	db=$1
+	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" $db | gzip > "$db-`date +%Y-%m-%d`".sql.gz
+}
+
 if [[ ${DB_USER} == "" ]]; then
 	echo "Missing DB_USER env variable"
 	exit 1
@@ -41,14 +47,13 @@ if [[ ${ALL_DATABASES} == "" ]]; then
 		echo "Missing DB_NAME env variable"
 		exit 1
 	fi
-	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" | gzip > "${DB_NAME}-`date +%Y-%m-%d`".sql.gz
+	dump_db $DB_NAME
 	rotate_dumps $DB_NAME
 else
 	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 	for db in $databases; do
 	    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-	        echo "Dumping database: $db"
-	        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db | gzip > "$db-`date +%Y-%m-%d`".sql.gz
+	        dump_db $db
 			rotate_dumps $db
 	    fi
 	done
